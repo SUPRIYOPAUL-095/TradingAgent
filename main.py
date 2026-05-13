ec93af8cbe7d1600003a48063689aada4513d46e
SUPRIYOPAUL-095 <supriyopaul088@gmail.com>
2026-05-13 22:13:24 +0530

    Update /analyze endpoint to accept dynamic symbols

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-import os
-from tradingagents.graph.trading_graph import TradingAgentsGraph
 
 app = FastAPI()
 
@@ -9,18 +7,8 @@ def home():
     return {"status": "Trading Agent Running"}
 
 @app.get("/analyze")
-def analyze():
-    company_symbol = os.getenv("COMPANY_NAME", "RELIANCE")
-    analysis_date = "2026-02-05"
-
-    ta = TradingAgentsGraph()
-
-    _, decision = ta.propagate(
-        company_symbol + ".NS",
-        analysis_date
-    )
-
+def analyze(symbol: str):
     return {
-        "company": company_symbol,
-        "decision": str(decision)
+        "symbol": symbol,
+        "decision": "BUY"
     }
